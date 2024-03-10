77c8f5d7c41247a08d5d0c7061bdcadd3b6db6de
woni928 <398401414@qq.com>
2024-03-10 08:01:16 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -54,3 +54,6 @@ rm -rf feeds/smpackage/luci-app-bypass
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 
+script/feeds clean
+scripts/feeds update -a
+scripts/feeds install -a
