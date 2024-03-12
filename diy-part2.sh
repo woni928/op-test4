026122ca3c77e5584152203b4e4701e491e618bf
woni928 <398401414@qq.com>
2024-03-12 09:36:51 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -47,10 +47,10 @@ sed -i 's/REENTRANT -D_GNU_SOURCE/LARGEFILE64_SOURCE/g' feeds/packages/lang/perl
 ./scripts/feeds update -a
 rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd-alt,miniupnpd-iptables,wireless-regdb}
 rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/luci/applications/luci-app-smartdns
-rm -rf feeds/packages/net/{alist,adguardhome,mosdns,smartdns}
-rm -rf feeds/smpackage/luci-app-adguardhome
-rm -rf feeds/smpackage/luci-app-bypass
+#rm -rf feeds/luci/applications/luci-app-smartdns
+#rm -rf feeds/packages/net/{alist,adguardhome,mosdns,smartdns}
+#rm -rf feeds/smpackage/luci-app-adguardhome
+#rm -rf feeds/smpackage/luci-app-bypass
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 ./scripts/feeds install -a
