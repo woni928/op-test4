9e817db0388876f0ca4f129acd02c3b8bc11ea9f
woni928 <398401414@qq.com>
2024-03-14 11:09:56 +0800

    2024-0314

--- diy-part2.sh
+++ diy-part2.sh
@@ -48,7 +48,3 @@ rm -rf feeds/packages/net/mosdns
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
-
-
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
